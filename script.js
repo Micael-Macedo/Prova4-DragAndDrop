01b1cb0d66ded77d24261b6b9828202265bf6d17
Micael-Macedo <micaelm2009@hotmail.com>
2023-04-11 12:01:27 -0300

    feat: add tutorial

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-function toggleSenha(){
+function toggleSenha() {
     let senha = $("#password");
     console.log(senha.get(0).type);
-    if(senha.get(0).type == "text"){
+    if (senha.get(0).type == "text") {
         $(senha).get(0).type = "password";
-    }else{
+    } else {
         $(senha).get(0).type = "text";
     }
 }
 function buscarProduto(produto, botao) {
     console.log(produto);
     $('.allPieces').empty()
-    if(produto == "machines"){
+    if (produto == "machines") {
         $('.createMachine').append(
             "<a href='criarMaquina.html'>Criar nova máquina</a> "
-            )
-        }else{
-            $(".createMachine").empty();
-        }
+        )
+    } else {
+        $(".createMachine").empty();
+    }
     $(".navList button").removeClass("active");
     $(botao).addClass("active");
-   let result = fetch(`localhost/alatech/api/${produto}`)
-   .then(r => {return r.json})
-   .catch(e => {return false});
-   if(result){
-    // result.forEach(produto => {        
-    //     $('#allProdutos').append(
-    //     `<div class="produto" title="Nome = ${produto.nome} Marca = ${produto.marca}">
-    //         <img src="${produto.img}" alt="">
-    //         <div>
-    //             <h2>${produto.id}</h2>
-    //             <button></button>
-    //         </div>
-    //     </div>`
-    //     );
-    // });
-    $('.allPieces').append(
-        `<div class="produto" title="Nome = Fontes Marca = Marca" value="1" onclick="selectProduto(this, '${produto}')" draggable="true" name="${produto}" ondragstart="dragstart(this)" ondrag="drag(this)" ondragend="dragend(this)">
+    let result = fetch(`localhost/alatech/api/${produto}`)
+        .then(r => { return r.json })
+        .catch(e => { return false });
+    if (result) {
+        // result.forEach(produto => {        
+        //     $('#allProdutos').append(
+        //     `<div class="produto" title="Nome = ${produto.nome} Marca = ${produto.marca}">
+        //         <img src="${produto.img}" alt="">
+        //         <div>
+        //             <h2>${produto.id}</h2>
+        //             <button></button>
+        //         </div>
+        //     </div>`
+        //     );
+        // });
+        $('.allPieces').append(
+            `<div class="produto" title="Nome = Fontes Marca = Marca" value="1" onclick="selectProduto(this, '${produto}')" draggable="true" name="${produto}" ondragstart="dragstart(this)" ondrag="drag(this)" ondragend="dragend(this)">
         <img src="images/1.png" alt="">
         <div>
         <h2>${produto} Bacana1</h2>
@@ -47,17 +47,17 @@ function buscarProduto(produto, botao) {
         <h2>${produto} Bacana2</h2>
         </div>
         </div>`
-    )
-   }
+        )
+    }
 }
 function listarProduto() {
     let produtoVal = $("#selectPecas").find(':selected').val();
     let result = fetch(`localhost/alatech/api/${produtoVal}`)
-   .then(r => {return r.json})
-   .catch(e => {return false});
-   $('#listPecas').empty();
-   $('#listPecas').append(
-    `<div class="produto" title="Nome = Fontes Marca = Marca" value="1" onclick="selectProduto(this, '${produtoVal}')" draggable="true" name="${produtoVal}" ondragstart="dragstart(this)" ondrag="drag(this)" ondragend="dragend(this)">
+        .then(r => { return r.json })
+        .catch(e => { return false });
+    $('#listPecas').empty();
+    $('#listPecas').append(
+        `<div class="produto" title="Nome = Fontes Marca = Marca" value="1" onclick="selectProduto(this, '${produtoVal}')" draggable="true" name="${produtoVal}" ondragstart="dragstart(this)" ondrag="drag(this)" ondragend="dragend(this)">
         <img src="images/1.png" alt="">
         <div>
         <h2>${produtoVal} Bacana1</h2>
@@ -69,11 +69,11 @@ function listarProduto() {
         <h2>${produtoVal} Bacana2</h2>
         </div>
         </div>`
-        );
-   if(result){
-        result.forEach(produto => {        
+    );
+    if (result) {
+        result.forEach(produto => {
             $('#listPecas').append(
-            `<div class="produto" title="Nome = ${produto.nome} Marca = ${produto.marca}" value="${produto.id}" onclick="selectProduto(this, '${produtoVal}')" draggable="true">
+                `<div class="produto" title="Nome = ${produto.nome} Marca = ${produto.marca}" value="${produto.id}" onclick="selectProduto(this, '${produtoVal}')" draggable="true">
                 <img src="${produto.img}" alt="">
                 <div>
                     <h2>${produto.nome}</h2>
@@ -91,45 +91,47 @@ statusFonte = 0
 function validarLogin() {
     let username = $('#username').val();
     let password = $('#password').val();
-    let body = { 
+    let body = {
         "username": username,
         "password": password
     }
     console.log(body);
-    if(body.username != "" && body.password != ""){
+    if (body.username != "" && body.password != "") {
         window.location.href = "http://127.0.0.1:5500/home.html";
         let result = fetch("localhost/alatech/api/login", body)
-            .then(r => {return r.json})
-            .catch(e => {return false})
-        if(result){
+            .then(r => { return r.json })
+            .catch(e => { return false })
+        if (result) {
         }
-    }else{
+    } else {
         $('.modal').show();
     }
 }
 $(document).ready(function () {
-        $('.modal').hide();
-        $(".navList li").html()
-        buscarProduto("machines");
+    $('.modal').hide();
+    $(".navList li").html()
+    openModal('#apresentacao')
+    buscarProduto("machines");
 });
-function closeModal(){
+function closeModal() {
     $('.modal').hide();
 }
 $("#selectPecas").on('change', function () {
     listarProduto();
 });
-function salvarMaquina(){
-    $("#imagemMaquina").show();
+function openModal(modal) {
+    closeModal()
+    $(modal).show(), 400
 }
-function selectProduto(Produto, tipo){
-   $(`#${tipo}`).append(Produto);
+function selectProduto(Produto, tipo) {
+    $(`#${tipo}`).append(Produto);
 }
 function verificarPecas() {
-    if( statusMae == 1 && statusFonte == 1){
+    if (statusMae == 1 && statusFonte == 1) {
         $("option").attr("disabled", false)
     }
 }
-function removerProduto(produto){
+function removerProduto(produto) {
     let button = $(produto).parent(".header").parent(".peca");
     let dropzone = $(button).find(".dropzone");
     $(listPecas).append(dropzone.firstElementChild)
@@ -147,7 +149,7 @@ function dragstart(item) {
 function drag() {
     console.log("card dragging")
 }
-function dragend(item){
+function dragend(item) {
     console.log("card end")
     item.classList.remove("is-dragging");
     let dropzonePlace = document.querySelector(".highlight")
@@ -158,31 +160,48 @@ dropzones.forEach(dropzone => {
     dropzone.addEventListener("dragover", dragover)
 });
 
-function dragenter(){
+function dragenter() {
     let pecaBeingDragging = document.querySelector(".is-dragging");
-    if(this.id == pecaBeingDragging.getAttribute("name")){
-        if(pecaBeingDragging.getAttribute("name") == "motherboard"){
+    if (this.id == pecaBeingDragging.getAttribute("name")) {
+        if (pecaBeingDragging.getAttribute("name") == "motherboard") {
             statusMae = 1;
-            dropzones.forEach(dropzone =>{
+            dropzones.forEach(dropzone => {
                 $(dropzone).empty();
             })
         }
-        if(pecaBeingDragging.getAttribute("name") == "power-supplies"){
+        if (pecaBeingDragging.getAttribute("name") == "power-supplies") {
             statusFonte = 1;
         }
-        if(this.children){
+        if (this.children) {
             $(listPecas).append(this.firstElementChild)
         }
         this.appendChild(pecaBeingDragging);
         verificarPecas()
     }
 }
-function dragover(){
+function dragover() {
     let pecaBeingDragging = document.querySelector(".is-dragging");
-    if(this.id == pecaBeingDragging.getAttribute("name")){
-        if(this.children){
+    if (this.id == pecaBeingDragging.getAttribute("name")) {
+        if (this.children) {
             $(listPecas).append(this.firstElementChild)
         }
         this.appendChild(pecaBeingDragging);
     }
 }
+
+const $file = document.querySelector("#arquivoMaquina");
+$file.addEventListener("change", (event) => {
+    const selectedfile = event.target.files;
+    if (selectedfile.length > 0) 
+    {
+        const [imageFile] = selectedfile;
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            const srcData = fileReader.result;
+            console.log('base64:', srcData)
+        };
+        fileReader.readAsDataURL(imageFile);
+    }
+});
+
+
